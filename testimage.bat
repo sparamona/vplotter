da11b2166372331cef32b7defb4b5fffa25f1cf6
jsheena <jsheena@gmail.com>
2020-03-20 23:46:00 -0400

    added filename

--- testimage.bat
+++ testimage.bat
@@ -1,5 +1,5 @@
-python3 .\Img2PlotXY.py .\images\test1a.jpg .\images\test1-xy.plot %1 %2 %3 %4
-set OUTFILE=.\images\output_%1_%2_%3_%4.png
-python3 .\Plot2Img.py .\images\test1-xy.plot %OUTFILE%
+python3 .\Img2PlotXY.py %1 .\images\intermediate-xy.plot %2 %3 %4 %5
+set OUTFILE=%1-out-_%2_%3_%4_%5.png
+python3 .\Plot2Img.py .\images\intermediate-xy.plot %OUTFILE%
 %OUTFILE%
 
