f66e8f2d994e9c8796fe4a5b4b418989de476602
jsheena <jsheena@gmail.com>
2020-04-01 15:46:32 -0400

    fixes to square

--- Square.py
+++ Square.py
@@ -4,17 +4,19 @@ from Plotter import Lengths,Point,Plotter
 
 
 
-start = Point( int(sys.argv[1]), int(sys.argv[2]) )
-size = Point( int(sys.argv[3]), int(sys.argv[4]) )
-plotfile = open(sys.argv[5],'w')
+size = Point( int(sys.argv[1]), int(sys.argv[2]) )
+plotfile = open(sys.argv[3],'w')
 
 
-plotarea = (250,300,942-250,650)
+margins = [ 250, 250, 250, 300] # left, top, right down
+totalarea = (1200, 1700)
+
+start=Point( totalarea[0]/2-size.x/2, margins[1]+300)
 
 plotter =Plotter(plotfile)
 
 plotter.reset()
-plotter.drawLineTo( start, True )
+plotter.drawLineTo( start, False )
 plotter.drawLineTo( Point( start.x+size.x, start.y) , True)
 plotter.drawLineTo( Point( start.x+size.x, start.y + size.y) , True)
 plotter.drawLineTo( Point( start.x, start.y + size.y) , True)
